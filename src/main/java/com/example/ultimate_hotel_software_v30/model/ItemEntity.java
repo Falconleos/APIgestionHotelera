d350cbed94e11df222d65fa49be2f2fac0e4cd66
Falconleos <leonelfsotopublic@gmail.com>
2026-08-21 11:03:42 -0300

    ajuste total cuenta

--- src/main/java/com/example/ultimate_hotel_software_v30/model/ItemEntity.java
+++ src/main/java/com/example/ultimate_hotel_software_v30/model/ItemEntity.java
@@ -17,7 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ItemEntity {
+public class    ItemEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
